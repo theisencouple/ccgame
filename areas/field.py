572d3ed1e5dd69ab4a6ebce879975724dfd16e36
Mark Theisen <mtheisen@soleaenergy.com>
2026-05-19 11:23:14 -0500

    showing we can use png in the minimap

--- areas/field.py
+++ areas/field.py
@@ -1,8 +1,18 @@
 from pathlib import Path
+import pygame
 from .base import Area
 from ui import colors
+from ui.ui import UI
 
 
 class Field(Area):
     def __init__(self) -> None:
         super().__init__("field", "An open field.", color=colors.FIELD, image=Path("assets/field.png"))
+
+    def render_minimap(self, ui: UI):
+        surf = ui.load_image(Path("assets/plains_16x16.png"), ui.TS, ui.TS).copy()
+        if not self.visited:
+            dim = pygame.Surface((ui.TS, ui.TS))
+            dim.fill((80, 80, 80))
+            surf.blit(dim, (0, 0), special_flags=pygame.BLEND_MULT)
+        return surf
--- areas/village.py
+++ areas/village.py
@@ -1,9 +1,19 @@
 from pathlib import Path
+import pygame
 from areas.base import Area
 from sections.base import Enterable
 from ui import colors
+from ui.ui import UI
 
 
 class Village(Area, Enterable):
     def __init__(self) -> None:
         super().__init__("village", "A small village.", color=colors.VILLAGE, image=Path("assets/village.png"))
+
+    def render_minimap(self, ui: UI):
+        surf = ui.load_image(Path("assets/village_16x16.png"), ui.TS, ui.TS).copy()
+        if not self.visited:
+            dim = pygame.Surface((ui.TS, ui.TS))
+            dim.fill((80, 80, 80))
+            surf.blit(dim, (0, 0), special_flags=pygame.BLEND_MULT)
+        return surf
